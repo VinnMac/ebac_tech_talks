dbf4920651f1f9af9f56873c984a669e60a6e277
Vinícius Machado <vinicius.machadoo@outlook.com>
2026-02-10 12:06:33 -0300

    Projeto com contador finalizado

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+AOS.init();
+
+const momentoDoEvento = new Date("Feb 10, 2026 11:58");
+const timestampDoEvento = momentoDoEvento.getTime();
+
+setInterval(function() {
+    const momentoAtual = new Date();
+    const timestampAtual = momentoAtual.getTime();
+
+    const tempoAteOEvento = timestampDoEvento - timestampAtual;
+
+    minutosEmMS = 1000 * 60
+    horasEmMS = minutosEmMS * 60
+    diaEmMS = horasEmMS * 24;
+    
+    diasAteOEvento = Math.floor(tempoAteOEvento / diaEmMS);
+    horasAteOEvento = Math.floor(tempoAteOEvento % diaEmMS / horasEmMS);
+    minutosAteOEvento = Math.floor(tempoAteOEvento % horasEmMS / minutosEmMS);
+    segundosAteOEvento = Math.floor(tempoAteOEvento % minutosEmMS / 1000)
+
+    const contador = document.getElementById("contador");
+    
+    contador.innerHTML = tempoAteOEvento > 0 
+    ? `Começa em ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s` : "encerrou";
+}, 1000)
